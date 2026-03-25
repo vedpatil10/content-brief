14f8f5c9486cc460d3b9e29a206bfbc2b67d0630
Vedant Patil <vedantpatil1450@gmail.com>
2026-03-25 15:36:29 +0530

    Move Excel export client-side

--- server/index.ts
+++ server/index.ts
@@ -17,13 +17,14 @@ declare module "http" {
 
 app.use(
   express.json({
+    limit: "10mb",
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
   }),
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
@@ -100,6 +101,5 @@ app.use((req, res, next) => {
 })();
 
 export default app;
-
 
 
